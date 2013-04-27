5901989a10ad55af914b1c505953c4ef3649dc5e
Jack <jack.huang78@gmail.com>
2013-04-27 13:15:12 -0700

    initial commit

---
+++ pa1/matmul.c
@@ -0,0 +1,14 @@
+// This is the matrix multiply kernel you are to replace.
+// Note that you are to implement C = C + AB, not C = AB!
+
+void matmul(int N, const double* A, const double* B, double* C) {
+  int i, j, k;
+  int ib_max, jb_max, kb_max;
+  int ib, jb, kb;
+  int B1 = 32;  
+  
+  for (i = 0; i < N; i++)  
+    for (j = 0; j < N; j++)  
+      for (k = 0; k < N; k++) 
+        C[i*N+j]+=A[i*N+k]*B[k*N+j];
+}
