a6414da09151fbe7ec0b96403cc12850840df5f4
Jack <jack.huang78@gmail.com>
2013-04-27 19:35:42 -0700

    implemented SSE2 instinsics

--- pa1/matmul.c
+++ pa1/matmul.c
@@ -1,6 +1,9 @@
 // This is the matrix multiply kernel you are to replace.
 // Note that you are to implement C = C + AB, not C = AB!
 
+#include <stdio.h>
+#include <xmmintrin.h>
+
 /*
 	#0: the original code; no optimization 
 */
@@ -15,9 +18,9 @@ void matmul0(int N, const double* A, const double* B, double* C) {
 }
 
 /*
-	#1: added blocking
+	#1: blocking
 */
-void matmul(int N, const double* A, const double* B, double* C) {
+void matmul1(int N, const double* A, const double* B, double* C) {
 	int i, j, k;
 	int ib_max, jb_max, kb_max;
 	int ib, jb, kb;
@@ -55,3 +58,52 @@ void matmul(int N, const double* A, const double* B, double* C) {
 
 
 }
+
+/*
+	SSE2 intrinsics & block_size 2
+*/
+void matmul(int N, const double* A, const double* B, double* C) {
+	int i, j, k;
+	int ii, jj, kk;
+	int b = 2;
+
+	__m128d a11, a12, a21, a22;
+	__m128d b1, b2;
+	__m128d c1, c2;
+
+	for(i = 0; i < N; i+=b)  
+		for(j = 0; j < N; j+=b) {
+
+			c1 = _mm_load_pd(&C[i*N+0+j]);			
+			c2 = _mm_load_pd(&C[i*N+N+j]);
+			
+			for(k = 0; k < N; k+=b) {
+
+				a11 = _mm_load1_pd(&A[i*N+0+k+0]);
+				a12 = _mm_load1_pd(&A[i*N+0+k+1]);
+				a21 = _mm_load1_pd(&A[i*N+N+k+0]);
+				a22 = _mm_load1_pd(&A[i*N+N+k+1]);
+
+				b1 = _mm_load_pd(&B[k*N+0+j]);
+				b2 = _mm_load_pd(&B[k*N+N+j]);
+
+				a11 = _mm_mul_pd(a11, b1);
+				a12 = _mm_mul_pd(a12, b2);
+				a21 = _mm_mul_pd(a21, b1);
+				a22 = _mm_mul_pd(a22, b2);
+			
+				c1 = _mm_add_pd(c1, a11);
+				c1 = _mm_add_pd(c1, a12);
+				c2 = _mm_add_pd(c2, a21);
+				c2 = _mm_add_pd(c2, a22);								
+				
+				//C[i*N+j]+=A[i*N+k]*B[k*N+j];
+			}
+			_mm_store_pd(&C[i*N+0+j], c1);
+			_mm_store_pd(&C[i*N+N+j], c2);
+		}
+
+	
+		
+}
+
